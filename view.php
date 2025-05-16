6342bc8cab96cec392366c176d6a7719d964e31b
Rahmat Mulia <rahmatzkk10@gmail.com>
2025-05-16 14:23:02 +0700

    Create view.php

---
+++ view.php
@@ -0,0 +1,18 @@
+  <script src="https://accounts.google.com/gsi/client" async defer></script>
+<div id="g_id_onload"
+     data-client_id="723388901536-bsglfjcmqch880c1gppvtfsb7uusi5v8.apps.googleusercontent.com"
+     data-login_uri="<?= site_url('auth/google_callback') ?>"
+     data-auto_prompt="false"
+     data-context="signin"
+     data-ux_mode="redirect"
+     data-itp_support="true">
+</div>
+
+<div class="g_id_signin"
+     data-type="standard"
+     data-size="large"
+     data-theme="outline"
+     data-text="sign_in_with"
+     data-shape="rectangular"
+     data-logo_alignment="left">
+</div>
